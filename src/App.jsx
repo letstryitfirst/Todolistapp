23dbb28e1a286106bbce0e7634e3383a7d68e62b
Prathmesh Sarwdnya <prathmeshsarwdnya@gmail.com>
2023-10-18 21:15:40 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import './components/Responsive.css'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
- 
+
   return (
     <div className='body'>
       <Router>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route  exact path='/' element={<Home/>}></Route>
-          <Route  exact path='/Todolistapp' element={<Home/>}></Route>
-          <Route  exact path='/Todolist' element={<Home/>}></Route>
-          <Route  exact path='/home' element={<Home/>}></Route>
-          <Route exact path='/about' element={<About/>}></Route>
+
+          <Route exact path='/Todolistapp' element={<Home />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+
         </Routes>
       </Router>
     </div>
